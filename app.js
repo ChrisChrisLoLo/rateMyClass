ba328aead928525d1ece2aedce2ff6b8f6c9a93b
ChrisChrisLoLo <sporewoh@gmail.com>
2018-04-02 20:32:14 -0600

    Added a rating field site, starting to add MongoDB integration

--- app.js
+++ app.js
@@ -2,6 +2,34 @@ const express = require('express');
 const hbs = require('hbs');
 var app = express();
 
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+//mongoose.connect('mogodb://localhost:27017/appData');
+
+var courseRating = mongoose.model('courseRating',{
+	//Possiblities: have prereqs/coreqs, postreqs
+	courseDep:{
+		type: String
+	},
+	courseNum:{
+		type: String
+	},
+	description:{
+		type: String
+	},
+	difficulty:{
+		type: Number
+	},
+	workload:{
+		type: Number
+	},
+	practicality:{
+		type: Number
+	},
+	enjoyment:{
+		type: Number
+	},
+});
 
 
 hbs.registerPartials(__dirname+"/views/partials")
@@ -13,12 +41,13 @@ app.get('/',function(req,res){
 		name: "Penelope"
 	});
 });
-
-app.get('/demo',function(req,res){
-	res.render('index.hbs',{
+app.get('/rate',function(req,res){
+	res.render('rate.hbs',{
 		name: "Penelope"
 	});
 });
+
+
 //function is a callback, but this function can do more if desired.
 app.listen(3000,function(){
 	console.log("Server Started on Port 3000...");
