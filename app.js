18540dfc78380df6e6bcb9c94b6aa7e59b6168e3
ChrisChrisLoLo <sporewoh@gmail.com>
2018-04-08 19:16:13 -0600

    Added search functionality

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/appData');
 
-var courseRating = mongoose.model('courseRating',{
+var CourseRating = mongoose.model('CourseRating',{
 	//Possiblities: have prereqs/coreqs, postreqs
 	courseDep:{
 		type: String,
@@ -75,14 +75,14 @@ app.get('/',function(req,res){
 		name: "Penelope"
 	});
 });
-app.get('/rate',function(req,res){
+app.get('/rateCourse',function(req,res){
 	res.render('rate.hbs',{
 		name: "Penelope",
 		errors: null
 	});
 });
 
-app.post('/submit_rating',(req,res)=>{
+app.post('/submitRating',(req,res)=>{
 	console.log(req.body)
 	req.checkBody('courseDep',"Course Department must be 3-5 characters long").isLength({min:3,max:5})
 		.isAlpha().withMessage("Course Department must only contain letters")
@@ -104,14 +104,16 @@ app.post('/submit_rating',(req,res)=>{
 		return;
 	}
 	else{
-		var formRating = new courseRating({
+		var overall = ((parseInt(req.body.difficulty)+parseInt(req.body.workload)+parseInt(req.body.practicality)+parseInt(req.body.enjoyment))/4)
+		var formRating = new CourseRating({
 			courseDep:req.body.courseDep.toUpperCase(),
 			courseNum:parseInt(req.body.courseNum),
 			difficulty:parseInt(req.body.difficulty),
 			workload:parseInt(req.body.workload),
 			practicality:parseInt(req.body.practicality),
 			enjoyment:parseInt(req.body.enjoyment),
-			comments:req.body.comments
+			comments:req.sanitize(req.body.comments),
+			overall:parseFloat(overall)
 		});
 		formRating.save().then(()=>{res.redirect('success');
 		},(e) => {
@@ -120,26 +122,37 @@ app.post('/submit_rating',(req,res)=>{
 	}
 });
 //Todo: bar anyone from this http if they didn't JUST submit a form
-app.get('/submit_rating/success',(req,res)=>{
+app.get('/submitRating/success',(req,res)=>{
 	res.render('rateSuccess.hbs');
-});
-
+})
 app.get('/viewCourse',(req,res)=>{
 	res.render('viewCourse.hbs');
 });
 app.post('/viewCourse',(req,res)=>{
-	// req.checkBody('courseDep',"Course Department must be 3-5 characters long").isLength({min:3,max:5})
-	// .isAlpha().withMessage("Course Department must only contain letters")
-	// .trim();
-	// var errors = req.validationErrors();
-	// console.log(req.body)
-	//if (errors) {
-	res.redirect('/viewCourse/'+req.body.courseDep+'/'+req.body.courseNum+'/')
+	res.redirect('/viewCourse'+"/"+req.body.courseDep.toUpperCase()+'/'+req.body.courseNum)
 });
 //TODO: GET CUSTOM ROUTE and process data
 app.get('/viewCourse/:courseDep/:courseNum',(req,res)=>{
-	res.render('rateSuccess.hbs');
+
+	CourseRating.find({
+		courseDep:req.params.courseDep,
+		courseNum:req.params.courseNum
+		},
+		(err,courseRatings)=>{
+			if (err){
+				//TODO: MAKE ERROR HBS
+				res.render('index.hbs');
+				return console.error(err);
+			}
+			console.log(typeof courseRatings)
+			console.log(courseRatings);
+			res.render('viewCourseSel.hbs',{courseDep:req.params.courseDep,
+											courseNum:req.params.courseNum,
+											entries:courseRatings,
+											});
+	});
 });
+
 //function is a callback, but this function can do more if desired.
 app.listen(3000,function(){
 	console.log("Server Started on Port 3000...");
